a070a29d20c1787e5210ff5aadc9af7ff4499a0c
KennaReed <mreed16@calpoly.edu>
2021-08-06 09:07:28 -0700

    Added printing epoch 30 data functionality

--- runModel.sh
+++ runModel.sh
@@ -8,19 +8,25 @@ valLossSum=0
 for (( i=1; i<=$1; i++))
    do 
       python src/weatherModel.py >> epochData.txt
-
+      
       loss=$(awk -v var="$lineCount" 'NR==var {print $18}' epochData.txt)
       lossSum=$(echo "$lossSum" + "$loss" | bc -l)
 
       valLoss=$(awk -v var="$lineCount" 'NR==var {print $24}' epochData.txt)
       valLossSum=$(echo "$valLossSum" + "$valLoss" | bc -l)
       lineCount=$(expr $lineCount + 60)
-      
-      echo "-------------------------------"
+
+      if [ "$#" == "2" ] && [ "$2" == "p" ]
+      then
+         echo " $i Trial Loss: $loss" 
+         echo " $i Trial Loss: $valLoss"
+      fi 
+
+      echo "-----------------------------------------------"
    done
 
 printf '\nAverage Loss: '
-awk "BEGIN {print $lossSum/6}"
+awk "BEGIN {print $lossSum/$i}"
 
 printf "\nAverage Validation Loss: "
-awk "BEGIN {print $valLossSum/6}"
+awk "BEGIN {print $valLossSum/$i}"
